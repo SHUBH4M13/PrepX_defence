26c56662c6c8466d9c7a4982a0800d2e7999a78e
Shubham Karna <skarna230@gmail.com>
2025-03-06 02:58:04 +0530

    heroText fully responsive

--- Frontend/src/Components/Sections/HomeSection/HeroText.jsx
+++ Frontend/src/Components/Sections/HomeSection/HeroText.jsx
@@ -5,34 +5,46 @@ import MacbookProcess from './MacBookProcess'
 
 export default function HeroText() {
     return (
-        <div className=' bg-darkbg w-full flex flex-col lg:flex-row  gap-4 md:gap-2 px-14 py-16'>
+        <div className=' bg-darkbg w-full flex flex-col lg:flex-row  gap-4 md:gap-2 px-4 sm:px-14 py-16 lg:gap-8'>
 
-            <div className='  px-16 flex flex-col animate-fade-in-left'>
+            <div className='flex flex-col animate-fade-in-left'>
 
                 <div>
                     <div>
 
-                        <div className='flex ' >
-                            <h1 className=' text-4xl text-center sm:text-left sm:text-6xl font-Helvetica text-PrimaryGold font-bold'>Conquer</h1>
-                            <h1 className=' px-3 text-4xl text-center sm:text-left sm:text-6xl font-Helvetica text-white font-bold '>
-                                Defence
-                            </h1>
+                        <div className='flex items-center justify-center sm:justify-start' >
+
+                            <div>
+                                <h1 className=' text-3xl text-center sm:text-left sm:text-6xl font-Helvetica text-PrimaryGold font-bold'>Conquer</h1>
+                            </div>
+                            <div>
+                                <h1 className='px-2 text-3xl text-center sm:text-left sm:text-6xl font-Helvetica text-white font-bold '>
+                                    Defence
+                                </h1>
+                            </div>
+
                         </div>
-                        <div>
-                            <h1 className='text-4xl  text-center sm:text-left sm:text-6xl  font-Helvetica font-bold text-white'>
+
+                        <h1 className='text-3xl  text-center sm:text-left sm:text-6xl font-Helvetica font-bold text-white'>
                                 Exams
                             </h1>
-                            <h1 className='text-4xl text-center sm:text-left  sm:text-6xl  font-Helvetica font-bold text-white underline decoration-darkgreen decoration-4 underline-offset-auto'>
-                                With Precision
-                            </h1>
+
+                        <div className='flex items-center justify-center sm:justify-start'>
+                            
+                            <div>
+                                <h1 className='text-3xl text-center sm:text-left  sm:text-6xl  font-Helvetica font-bold text-white'>
+                                    With Precision
+                                </h1>
+                            </div>
                         </div>
+
                         <div>
                             <p className=' py-5 text-xl text-center sm:text-left text-white/90 font-Helvetica '> Prepare for NDA, CDS, AFCAT, and more with military-grade mock tests designed to ensure your success. Train like a warrior,
                                 succeed like a champion. </p>
                         </div>
                     </div>
 
-                    <div className=' flex justify-center gap-5 py-5 '>
+                    <div className=' flex justify-center sm:justify-start gap-5 py-5 '>
                         <div className='  bg-PrimaryGold px-4 sm:px-10 font-Helvetica font-bold hover:bg-hovergold duration-300 rounded-xl flex justify-center items-center'>
                             <button className=' text-darkbg p-2.5 sm:p-6 font-Helvetica font-bold  text-md rounded-xl'>Start Training Now</button>
                             <ArrowRightIcon className='size-8 sm:size-4 font-bold text-Secondarybg' />
@@ -45,7 +57,7 @@ export default function HeroText() {
 
                     </div>
 
-                    <div className=' px-10 sm:px-20 sm:py-3 '>
+                    <div className=' py-6 opacity-0 sm:opacity-100'>
                         <div className=' flex gap-8 justify-center lg:justify-start '>
                             <TickInfoCard text=" AI based results" />
                             <TickInfoCard text=" AI based results" />
@@ -53,10 +65,10 @@ export default function HeroText() {
                     </div>
 
                 </div>
-                
+
             </div>
 
-            <div className=' flex justify-center lg:justify-start animate-fade-in-right'>
+            <div className='flex justify-center lg:justify-start animate-fade-in-right'>
                 <MacbookProcess />
             </div>
 
